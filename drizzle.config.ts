e2b2b685012fcbcf780c3db273086f814417eb86
Fabricio Carvalho <fcarvalho1999@gmail.com>
2025-08-19 13:48:13 -0300

    drizzle config

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "drizzle-kit";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("The DATABASE_URL env is required.");
+}
+
+export default defineConfig({
+  dialect: "postgresql",
+  casing: "snake_case",
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+  schema: "./src/database/schema/**.ts",
+  out: "./drizzle",
+});
