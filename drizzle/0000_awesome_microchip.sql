585d99993b4ba77c22ea4967cb326267daf9b7ab
Fabricio Carvalho <fcarvalho1999@gmail.com>
2025-08-19 14:37:14 -0300

    first migration

---
+++ drizzle/0000_awesome_microchip.sql
@@ -0,0 +1,13 @@
+CREATE TYPE "public"."user_role" AS ENUM('admin', 'instructor', 'student');--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" varchar(100) NOT NULL,
+	"email" varchar(255) NOT NULL,
+	"password" varchar(255) NOT NULL,
+	"role" "user_role" DEFAULT 'student' NOT NULL,
+	"is_verified" boolean DEFAULT false NOT NULL,
+	"profile_img" varchar(500),
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
